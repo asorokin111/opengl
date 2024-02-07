a3a0a764f572c499a6483b04b2b4959f5dc91b9a
Artem Sorokin <Artem.Sorokin@vuoksi.fi>
2024-02-07 15:35:57 +0200

    Fixed the light scene

--- chapter2/ch2_main.cpp
+++ chapter2/ch2_main.cpp
@@ -149,6 +149,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
+        lightingShader.use();
         lightingShader.setVec3("objectColor", {0.0f, 0.5f, 0.7f});
         lightingShader.setVec3("lightColor",  {1.0f, 1.0f, 1.0f});
 
