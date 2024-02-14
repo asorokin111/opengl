eaec47c9e72f0d58d0d53006a7b61318e2f237c6
Artem Sorokin <Artem.Sorokin@vuoksi.fi>
2024-02-14 15:21:32 +0200

    Directional light with multiple objects on the scene

--- chapter2/ch2_main.cpp
+++ chapter2/ch2_main.cpp
@@ -119,6 +119,18 @@ int main()
     -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
+    constexpr glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -181,11 +193,6 @@ int main()
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", camera.view);
         sceneLight.direction = camera.view * lightDir;
-        glm::mat4 model = glm::mat4(1.0f);
-        lightingShader.setMat4("model", model);
-
-        glm::mat3 normalMatrix = glm::mat3(glm::inverseTranspose(camera.view * model));
-        lightingShader.setMat3("normalMatrix", normalMatrix);
 
         lightingShader.setMaterial(texturedMat);
         diffuse.bindTexture();
@@ -194,7 +201,18 @@ int main()
         lightingShader.setLight(sceneLight);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            glm::mat3 normalMatrix = glm::mat3(glm::inverseTranspose(camera.view * model));
+            lightingShader.setMat3("normalMatrix", normalMatrix);
+            lightingShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         /*lightCubeShader.use();
         lightCubeShader.setVec3("lightColor", lightColor);
